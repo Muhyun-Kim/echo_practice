bc9c428083936955af14e666aacc524bd5d95440
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2024-06-25 13:58:48 +0900

    change login logic

--- my-echo-front/app/lib/getLoginState.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-
-import getSession from './session';
-
-export async function getLoginState() {
-  const session = await getSession();
-  return !!session.id;
-}
--- my-echo-front/app/login/page.tsx
+++ my-echo-front/app/login/page.tsx
@@ -1,19 +1,39 @@
 'use client';
 
 import { Box, Button, Container, TextField, Typography } from '@mui/material';
-import React from 'react';
-import { useFormState } from 'react-dom';
-import { login } from './actions';
+import React, { useState } from 'react';
+import { apiFetch } from '../lib/api';
+import { redirect } from 'next/dist/server/api-utils';
+import { useRouter } from 'next/navigation';
 
 export default function Login() {
-  const [state, dispatch] = useFormState(login, null);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = { email, password };
+    const loginUrl = `${process.env.NEXT_PUBLIC_API_URL}/user/login`;
+    try {
+      const res = await apiFetch(loginUrl, {
+        method: 'POST',
+        body: JSON.stringify(data),
+      });
+      if (res.message === 'Login successful') {
+        router.push('/profile');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <Container component="main">
       <Box className="mt-8 flex flex-col items-center">
         <Typography component="h1" variant="h5">
           Log In
         </Typography>
-        <Box component="form" action={dispatch}>
+        <Box component="form" onSubmit={handleSubmit}>
           <TextField
             margin="normal"
             required
@@ -23,6 +43,8 @@ export default function Login() {
             name="email"
             autoComplete="email"
             autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <TextField
             margin="normal"
@@ -32,6 +54,9 @@ export default function Login() {
             label="Password"
             name="password"
             autoComplete="current-password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <Button type="submit" fullWidth variant="contained">
             Log In
